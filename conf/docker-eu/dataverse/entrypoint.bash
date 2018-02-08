c98d4928e871491d90d0050c17650247a27f8c51
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-02-08 14:39:18 +0100

    Setup Dataverse only if it's not installed

--- conf/docker-eu/dataverse/entrypoint.bash
+++ conf/docker-eu/dataverse/entrypoint.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ ! -e "/opt/glassfish4/glassfish/domains/domain1/applications/dataverse/WEB-INF/classes/Bundle.properties" ]; then
+	cd /opt/dv
+	./setupIT.bash
+fi
 cd /opt/glassfish4
 bin/asadmin stop-domain
 bin/asadmin start-domain
