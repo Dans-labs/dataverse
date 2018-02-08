d357f74350bebe256dda1cc069a140383b37d41a
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-02-08 15:21:41 +0100

    Added extra steps

--- conf/docker-eu/initial.bash
+++ conf/docker-eu/initial.bash
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-mkdir data
-mkdir data/db
-mkdir data/solr
+if [ ! -e data ]; then
+	mkdir data
+	mkdir data/db
+	mkdir data/solr
+fi
+cd dataverse
+./step1.sh
+./step2.sh
+
 #docker-compose build postgres
 docker-compose start postgres
 #docker-compose build solr
