dcbe3ab0bb4ecabeb8e3eca4cf04d54ac3ad026e
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2018-02-12 10:47:54 +0100

    Fixed

--- conf/docker-eu/dataverse/step2.sh
+++ conf/docker-eu/dataverse/step2.sh
@@ -13,14 +13,13 @@ cp ../../jhove/jhove.conf testdata/
 
 # not using dvinstall.zip for setupIT.bash; but still used in install.bash for normal ops
 wdir=`pwd`
-wdir=$wdir"/dataverse"
-cd ./dataverse
+#wdir=$wdir"/dataverse"
 echo $wdir
 cd $wdir
-if [ ! -e $wdir/dv/deps/dvinstall.zip ]; then
-	wget https://github.com/IQSS/dataverse/releases/download/v4.8.5/dvinstall.zip -O $wdir/dv/deps/dvinstall.zip
+if [ ! -e $wdir/dataverse/dv/deps/dvinstall.zip ]; then
+	wget https://github.com/IQSS/dataverse/releases/download/v4.8.5/dvinstall.zip -O $wdir/dataverse/dv/deps/dvinstall.zip
 fi
 
-if [ ! -e $wdir/dv/deps/dataverse.war ]; then
- 	wget https://github.com/IQSS/dataverse/releases/download/v4.8.5/dataverse-4.8.5.war -O $wdir/dv/deps/dataverse.war
+if [ ! -e $wdir/dataverse/dv/deps/dataverse.war ]; then
+ 	wget https://github.com/IQSS/dataverse/releases/download/v4.8.5/dataverse-4.8.5.war -O $wdir/dataverse/dv/deps/dataverse.war
 fi
